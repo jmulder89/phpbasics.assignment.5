de90454518651e4a12683bc4ba84aa9d6420be78
Jeroen Mulder <jeroen.mulder.1989@gmail.com>
2016-12-16 12:10:10 +0100

    Fished 01date.php of assignment 5 for PHP.

--- 01date.php
+++ 01date.php
@@ -1,4 +1,16 @@
 <?php
+//here i check if the input field is set and what to do if so.
+if(isset($_POST['date'])){
+    $input = $_POST['date'];
+
+    //I could not figure out how to show the difference in days so i took this method from stackoverflow.
+    //<b>http://stackoverflow.com/questions/2040560/finding-the-number-of-days-between-two-dates</b>
+    $now = time();
+    $your_date = strtotime($_POST['date']);
+    $dateDiff = $now - $your_date;
+}
+
+
 
 ?>
 <!DOCTYPE html>
@@ -10,14 +22,13 @@
 
 </head>
 <body>
-    <form>
-        Date:	<input type="text" name="date" value="" /><br/><br/>
-
+    <form action="01date.php" method="post">
+        Date:	<input type="date" name="date" value="<?php if(isset($_POST['date'])) { echo $_POST['date']; } ?>"  /><br/><br/>
                 <input type="submit" name="button" value="Submit">
 </form>
-    <?php
-
-    ?>
+<?php
+    echo floor($dateDiff / (60 * 60 * 24));
+?>
 </body>
 
 </html>
